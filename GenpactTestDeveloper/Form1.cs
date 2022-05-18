1515cdb5470ef8b10fbbb01ffd194af6a62abe5c
Dina Rodríguez <68084694+Rod19566@users.noreply.github.com>
2022-05-17 23:36:05 -0600

    Add files via upload

--- GenpactTestDeveloper/Form1.cs
+++ GenpactTestDeveloper/Form1.cs
@@ -13,6 +13,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+//using Spire.Xls;
+using Aspose.Cells;
 
 
 namespace GenpactTestDeveloper
@@ -25,7 +27,7 @@ namespace GenpactTestDeveloper
         }
         //GLOBALES
         FileStream v;
-        string dir, newdir, newfile;
+        string dir, newdir, newdirexcel, newfile, processedfolder;
         int cont = 0;
         Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
         Microsoft.Office.Interop.Excel.Workbook MASTERwb;
@@ -39,11 +41,7 @@ namespace GenpactTestDeveloper
         { //select the file
             OpenFileDialog opfile = new OpenFileDialog();
             FolderBrowserDialog opfolder = new FolderBrowserDialog();
-            /* if (opfile.ShowDialog() == DialogResult.OK)
-             {
-                 textBox1.Text = Path.GetFullPath(opfile.FileName);
-             }
-             */
+            
             if (opfolder.ShowDialog() == DialogResult.OK)
             {
                 //Gets address
@@ -54,55 +52,92 @@ namespace GenpactTestDeveloper
 
             //Monitor
             FileSystemWatcher watcher = new FileSystemWatcher(@dir);
-             //Selects filters for notification 
+            //Selects filters for notification 
             watcher.NotifyFilter = NotifyFilters.Attributes
                              | NotifyFilters.CreationTime
-                             /*
                              | NotifyFilters.DirectoryName 
                              | NotifyFilters.FileName
                              | NotifyFilters.LastAccess
                              | NotifyFilters.LastWrite
-                             | NotifyFilters.Security */
+                             | NotifyFilters.Security 
                              | NotifyFilters.Size;
-            
+
             watcher.EnableRaisingEvents = true;
-        
+
 
             watcher.Changed += Action2FolderPath;
             watcher.Created += Action2FolderPath;
-            /*
             watcher.Deleted += Action2FolderPath;
             watcher.Renamed += Action2FolderPath;
-            */
-            ProcessDirectory(dir);
-            var result1 = MessageBox.Show(cont.ToString(), "Cuenta",
-                                 MessageBoxButtons.YesNo,
-                                 MessageBoxIcon.Question);
 
+            ProcessDirectory(dir);
+            MASTERwb.Close();
+            xlApp.Quit();
+
+            ProcessExcels(processedfolder);
+            /*
+        var result1 = MessageBox.Show(cont.ToString(), "Cuenta",
+                             MessageBoxButtons.YesNo,
+                             MessageBoxIcon.Question);
+            */
 
         } //end Open Folder
 
         private void Action2FolderPath(object sender, FileSystemEventArgs e)
-        {
+        {/*
             var result = MessageBox.Show("Change", "Message",
                                  MessageBoxButtons.YesNo,
                                  MessageBoxIcon.Question);
+            */
+            ProcessDirectory(dir);
+            MASTERwb.Close();
+            xlApp.Quit();
 
+            ProcessExcels(processedfolder);
 
         }
         // Process all files in the directory passed in, recurse on any directories
         // that are found, and process the files they contain.
         public void ProcessDirectory(string targetDirectory)
-        {   
+        {
             // Process the list of files found in the directory.
             string[] fileEntries = Directory.GetFiles(targetDirectory);
             foreach (string fileName in fileEntries)
             {
                 ProcessFile(fileName);
-               // cont++; checking it co
+                // cont++; checking it co
             }
 
         }
+
+        public void ProcessExcels(string pathexcel) //Combines excels
+        {
+            FileInfo fi1 = new FileInfo(pathexcel);
+            // Get file extension   
+            string extn = fi1.Extension;
+            //string filename = fi1.Name;
+
+            string[] fileEntries = Directory.GetFiles(pathexcel);
+            Workbook SourceBook1 = new Workbook(newdirexcel);
+
+            foreach (string fileName in fileEntries)
+            {
+                if (fileName != newdirexcel) //No añade repeticion de MASTERwb
+                {
+                    Workbook SourceBook2 = new Workbook(fileName);
+                    SourceBook1.Combine(SourceBook2);
+                    // newWB.Worksheets.AddCopy(sheet, WorksheetCopyType.CopyAll);
+
+                    SourceBook1.Save(newdirexcel);
+
+                }
+
+            }
+
+            //newWB.SaveToFile(@newdirexcel, ExcelVersion.Version2013);
+
+        }
+
         public void ProcessFile(string path) //process once files are checked
         {
             FileInfo fi = new FileInfo(path);
@@ -111,13 +146,14 @@ namespace GenpactTestDeveloper
             string filename = fi.Name;
             textBox2.Text = extn;
 
-            if (extn != ".txt") //compares extension
+            if (extn != ".xls" && extn != ".xlsx") //compares extension
             {
                 folderCreateMove(dir, "NotApplicable", filename, path);
             }
             else
             {
                 folderCreateMove(dir, "Processed", filename, path);
+                processedfolder = newdir;
                 if (xlApp == null) //checks is excel is installed
                 {
                     MessageBox.Show("Excel is not properly installed!!");
@@ -127,12 +163,13 @@ namespace GenpactTestDeveloper
                 {
                     MASTERwb = xlApp.Workbooks.Add(Type.Missing); //creates new workbook
                                                                   //
-                    newdir = System.IO.Path.Combine(newdir, "MASTERwb.xlsx"); //Creates excel path
-                                                                              //Changes excel name
+                    newdirexcel = System.IO.Path.Combine(newdir, "MASTERwb.xlsx"); //Creates excel path
+                    
+                    //Changes excel name
                     object missing = System.Reflection.Missing.Value;
                     try
                     {
-                        MASTERwb.SaveAs(@newdir, Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook, missing, missing,
+                        MASTERwb.SaveAs(@newdirexcel, Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook, missing, missing,
                         false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
                         missing, missing, missing, missing, missing);
 
@@ -141,12 +178,13 @@ namespace GenpactTestDeveloper
                     {
                         //File Exists, will not create
                     }
+
                     //MASTERwb.Save(); //Saves file addition
                 }
             }
         }
         //Creates folder
-        private void folderCreateMove(string OGaddress, string FolderName, string FileName, string filepath )
+        private void folderCreateMove(string OGaddress, string FolderName, string FileName, string filepath)
         {
             //obtains new address for new file in new folder
             newdir = System.IO.Path.Combine(OGaddress, FolderName);
