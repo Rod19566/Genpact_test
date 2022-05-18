aebe5ed1a20a6019bf08b430968059f5b10473f9
Dina Rodríguez <68084694+Rod19566@users.noreply.github.com>
2022-05-17 18:38:24 -0600

    Add files via upload

--- GenpactTestDeveloper/Form1.cs
+++ GenpactTestDeveloper/Form1.cs
@@ -1,7 +1,7 @@
 ﻿
 /* Creador: Dina Rodríguez 
  * 
- * Programa: 
+ * Programa: Monitors a folder
  * */
 using System;
 using System.Collections.Generic;
@@ -23,22 +23,28 @@ namespace GenpactTestDeveloper
         {
             InitializeComponent();
         }
+        //GLOBALES
         FileStream v;
         string dir;
 
-
-
+        /*
+        static void Main()
+        {
+            
+        } //End MAIN
+        */
         private void button1_Click(object sender, EventArgs e)
         { //select the file
             OpenFileDialog opfile = new OpenFileDialog();
             FolderBrowserDialog opfolder = new FolderBrowserDialog();
-           /* if (opfile.ShowDialog() == DialogResult.OK)
-            {
-                textBox1.Text = Path.GetFullPath(opfile.FileName);
-            }
-            */
+            /* if (opfile.ShowDialog() == DialogResult.OK)
+             {
+                 textBox1.Text = Path.GetFullPath(opfile.FileName);
+             }
+             */
             if (opfolder.ShowDialog() == DialogResult.OK)
             {
+                //Gets address
                 textBox1.Text = Path.GetFullPath(opfolder.SelectedPath);
             }
             dir = Convert.ToString(textBox1.Text);
@@ -47,8 +53,35 @@ namespace GenpactTestDeveloper
             // Get file extension   
             string extn = fi.Extension;
             textBox2.Text = extn;
-            
 
+            //Monitor
+            FileSystemWatcher watcher = new FileSystemWatcher(@dir);
+
+            watcher.NotifyFilter = NotifyFilters.Attributes
+                             | NotifyFilters.CreationTime
+                             | NotifyFilters.DirectoryName
+                             | NotifyFilters.FileName
+                             | NotifyFilters.LastAccess
+                             | NotifyFilters.LastWrite
+                             | NotifyFilters.Security
+                             | NotifyFilters.Size;
+            
+            watcher.EnableRaisingEvents = true;
+        
+
+            watcher.Changed += Action2FolderPath;
+            watcher.Created += Action2FolderPath;
+            watcher.Deleted += Action2FolderPath;
+            watcher.Renamed += Action2FolderPath;
+
+
+        }
+
+        private void Action2FolderPath(object sender, FileSystemEventArgs e)
+        {
+            var result = MessageBox.Show("Change", "Message",
+                                 MessageBoxButtons.YesNo,
+                                 MessageBoxIcon.Question);
         }
 
         private void label1_Click(object sender, EventArgs e)
